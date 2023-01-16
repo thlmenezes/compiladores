2753fda4021c4898b9c5dfba9d7911cc89406fd4
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-01-16 13:48:50 -0300

    Make scripts scriptier

--- run-test.sh
+++ run-test.sh
@@ -4,7 +4,10 @@ TEST_FILE_SUFFIX="txt"
 TEST_FILENAME=$(basename "$1" ".$TEST_FILE_SUFFIX")
 
 DUMP_FOLDER=/tmp/compiladores-dump
-mkdir "$DUMP_FOLDER" -p
+if [ ! -d "$DUMP_FOLDER" ]; then
+  mkdir "$DUMP_FOLDER" -p
+fi
+
 DUMP_FILE="$DUMP_FOLDER/$TEST_FILENAME.$TEST_FILE_SUFFIX"
 
 if cat "$1" | ./parser &> "$DUMP_FILE"; then
