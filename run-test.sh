f6112059f898ba556de0ae6cdd00ad8cdcffd073
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-01-30 17:52:08 -0300

    Add -v flag to scripts

--- run-test.sh
+++ run-test.sh
@@ -3,6 +3,8 @@
 TEST_FILE_SUFFIX="txt"
 TEST_FILENAME=$(basename "$1" ".$TEST_FILE_SUFFIX")
 
+VERBOSE_FLAG=${2-:""}
+
 DUMP_FOLDER=/tmp/compiladores-dump
 if [ ! -d "$DUMP_FOLDER" ]; then
   mkdir "$DUMP_FOLDER" -p
@@ -12,7 +14,12 @@ DUMP_FILE="$DUMP_FOLDER/$TEST_FILENAME.$TEST_FILE_SUFFIX"
 
 if ./parser < "$1" &> "$DUMP_FILE"; then
   echo "Processing \"$TEST_FILENAME\": Ok!"
-  # cat "$DUMP_FILE"
+  
+  # if should print
+  if [ $VERBOSE_FLAG == "-v" ]; then
+    cat "$DUMP_FILE"
+  fi
+
 else
   echo "Processing \"$TEST_FILENAME\": ERROR!"
   cat "$DUMP_FILE"
--- run-tests.sh
+++ run-tests.sh
@@ -5,5 +5,5 @@ echo "Rodando testes..."
 TEST_FILES=./tests/*
 for test in $TEST_FILES
 do
-	./run-test.sh "$test"
+	./run-test.sh "$test" $@
 done
