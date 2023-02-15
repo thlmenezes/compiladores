88a60faef646ee691116b47f0ca2f274ade45561
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-02-15 15:54:25 -0300

    .

---
+++ result.asm
@@ -0,0 +1,19 @@
+li s0, 0
+li a7, 5
+ecall
+mv s0, a0
+li s1, 1
+WHILE_LOOP_19668864:
+mv t2, s0
+beqz t2, WHILE_LOOP_END_19668864
+mul t0, s1, s0
+mv s1, t0
+li s2, 1
+sub t1, s0, s2
+mv s0, t1
+j WHILE_LOOP_19668864
+WHILE_LOOP_END_19668864:
+mv t2, s1
+li a7, 1
+mv a0, t2
+ecall
